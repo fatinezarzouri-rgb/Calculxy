c4fdee88e96a4b898cfb1798d2976c445cef9b00
fatinezarzouri-rgb <fatine.zarzouri@enim.ac.ma>
2026-05-07 14:16:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import streamlit as st
 import easyocr
+import numpy as np
+from PIL import Image
 
-st.title("TEST OK")
 
-reader = easyocr.Reader(["en"], gpu=False)
+st.set_page_config(page_title="TEST OCR")
 
-st.success("EasyOCR chargé")
+st.title("TEST EASYOCR")
+
+
+@st.cache_resource
+def load_reader():
+    return easyocr.Reader(
+        ["en"],
+        gpu=False
+    )
+
+
+reader = load_reader()
+
+st.success("EasyOCR chargé avec succès")
+
+
+uploaded = st.file_uploader(
+    "Upload image",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded:
+
+    image = Image.open(uploaded).convert("RGB")
+
+    st.image(image)
+
+    arr = np.array(image)
+
+    results = reader.readtext(
+        arr,
+        detail=1
+    )
+
+    st.write(results)
